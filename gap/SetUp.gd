7288b20632a49e51ad8e3014cb2a795fa743dd68
MWhybrow92 <mlw10@ic.ac.uk>
2017-10-14 16:31:24 +0100

    New set up files

---
+++ gap/SetUp.gd
@@ -0,0 +1,19 @@
+DeclareGlobalFunction( "MAJORANA_SetUp" );
+
+DeclareGlobalFunction( "MAJORANA_PairConjElements" );
+
+DeclareGlobalFunction( "MAJORANA_PairOrbits" );
+
+DeclareGlobalFunction( "MAJORANA_PairRepresentatives" );
+
+DeclareGlobalFunction( "MAJORANA_LongCoordinates" );
+
+DeclareGlobalFunction( "MAJORANA_MakeVector" );
+
+DeclareGlobalFunction( "MAJORANA_SetupOrbitals" );
+
+DeclareGlobalFunction( "MAJORANA_SetupOrbits" );
+
+DeclareGlobalFunction( "MAJORANA_ConnectedComponents" );
+
+DeclareGlobalFunction( "MAJORANA_DihedralProducts" );
