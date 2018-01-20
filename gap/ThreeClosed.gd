8a1d6bc64b1ea9774a0db88109be1abcfbd45823
MWhybrow92 <mlw10@ic.ac.uk>
2018-01-20 13:31:02 +0000

    Removed unused TC funcs from .gd

--- gap/ThreeClosed.gd
+++ gap/ThreeClosed.gd
@@ -4,38 +4,3 @@ DeclareGlobalFunction( "MAJORANA_ThreeClosedExtendPerm" );
 
 DeclareGlobalFunction( "MAJORANA_ThreeClosedFindVectorPermutation" );
 
-DeclareGlobalFunction( "ThreeClosedMajorana" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedFusion" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedProduct" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedExtractUnknownProducts" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedSeparateProduct" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedAxiomM1" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedEigenvectorsAlgebraUnknowns" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedResurrection" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedSolutionProducts" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedOrthogonality" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedFuseEigenvectors" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedFindBadIndices" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedAllEvecs" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedTestOrthogonality" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedTestEvecs" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedTestAxiomM1" );
-
-DeclareGlobalFunction( "MAJORANA_ThreeClosedTestFusion" );
-
-DeclareGlobalFunction( "ThreeClosedAlgebraTest" );
