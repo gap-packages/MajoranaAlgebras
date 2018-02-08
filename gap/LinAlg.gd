b824e465b775c43dabcbb71bb7da96ab57262e41
MWhybrow92 <mlw10@ic.ac.uk>
2018-02-08 10:23:25 +0000

    Removed reversed echelon form func

--- gap/LinAlg.gd
+++ gap/LinAlg.gd
@@ -2,13 +2,7 @@ DeclareGlobalFunction( "MAJORANA_LDLTDecomposition" );
 
 DeclareGlobalFunction( "MAJORANA_SolutionMatVecs" );
 
-DeclareGlobalFunction( "MAJORANA_SolutionMatVecs1" );
-
 DeclareGlobalFunction( "MAJORANA_PositiveDefinite" );
-
-DeclareGlobalFunction( "MAJORANA_NullSpace" );
-
-DeclareGlobalFunction( "MAJORANA_ReversedEchelonForm" );
 
 DeclareGlobalFunction( "_FoldList2" );
 
