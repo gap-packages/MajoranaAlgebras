75bb9bb193f63ef0f359b6ce9fbb4262042373ac
Markus Pfeiffer <markus.pfeiffer@st-andrews.ac.uk>
2018-10-18 10:53:59 +0100

    Some cleanup on orbits code

--- gap/Orbits.gd
+++ gap/Orbits.gd
@@ -6,3 +6,14 @@ DeclareGlobalFunction( "MAJORANA_OrbitalsT" );
 
 DeclareGlobalFunction( "MAJORANA_Orbits" );
 
+DeclareGlobalFunction( "MAJORANA_OrbitalStructure" );
+
+DeclareGlobalFunction( "MAJORANA_OrbitalRep" );
+DeclareGlobalFunction( "MAJORANA_OrbitalRepAct" );
+DeclareGlobalFunction( "MAJORANA_OrbitalStructureSigned" );
+DeclareGlobalFunction( "MAJORANA_OrbitalRepUnion" );
+DeclareGlobalFunction( "MAJORANA_OrbitalRepUnions" );
+DeclareGlobalFunction( "MAJORANA_OrbitalRepActSigned" );
+
+
+
