f8132d6cbcb7c478ac7a0693b7cef2582f447b1e
MWhybrow92 <mlw10@ic.ac.uk>
2018-11-19 15:21:57 +0100

    Fixed problem with declarations

--- gap/SignedPerm.gd
+++ gap/SignedPerm.gd
@@ -1,9 +1,27 @@
 # Signed permutations
 
 DeclareCategory("IsSignedPerm",
-                IsMultiplicativeElement and IsMultiplicativeElementWithOne and IsMultiplicativeElementWithInverse );
-BindGlobal("SignedPermFamily", NewFamily("SignedPermFamily"));
+                IsAssociativeElement and
+                IsExtLElement and
+                IsExtRElement and
+                IsMultiplicativeElement and
+                IsMultiplicativeElementWithOne and
+                IsMultiplicativeElementWithInverse and
+                IsFiniteOrderElement );
+
+BindGlobal("SignedPermFamily", NewFamily("SignedPermFamily", IsSignedPerm));
+
+DeclareCategoryCollections( "IsSignedPerm" );
+DeclareCategoryCollections( "IsSignedPermCollection" );
+
+DeclareConstructor( "NewSignedPerm", [ IsSignedPerm, IsList ] );
+DeclareConstructor( "NewSignedPerm", [ IsSignedPerm, IsPerm, IsList ] );
+
+
 DeclareRepresentation("IsSignedPermRep", IsSignedPerm and IsPositionalObjectRep, []);
 BindGlobal("SignedPermType", NewType(SignedPermFamily, IsSignedPermRep));
+
+DeclareRepresentation("IsSignedPermListRep", IsSignedPerm and IsPositionalObjectRep, []);
+BindGlobal("SignedPermListType", NewType(SignedPermFamily, IsSignedPermListRep));
 
 DeclareGlobalFunction("OnPosPoints");
