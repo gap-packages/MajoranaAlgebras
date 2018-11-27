3ff41550353c9162d5e367190ec7fc93023d13db
MWhybrow92 <mlw10@ic.ac.uk>
2018-11-27 11:21:00 +0100

    Removed obsolete orbitals code

--- gap/Orbits.gd
+++ gap/Orbits.gd
@@ -1,9 +1,3 @@
 DeclareGlobalFunction( "MAJORANA_FindOrbitals" );
 
-DeclareGlobalFunction( "MAJORANA_Orbitals" );
-
-DeclareGlobalFunction( "MAJORANA_NewOrbital" );
-
-DeclareGlobalFunction( "MAJORANA_OrbitalsT" );
-
 DeclareGlobalFunction( "MAJORANA_Orbits" );
